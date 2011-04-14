f34df260d8190d0f6c5a9c07ef7ffd8f2ce801f9
roger@kepler <roger@kepler>
2011-04-14 16:23:30 -0400

    Added NotImplementedException that extends RuntimeException. Provides similar syntactical sugar as .NET's class of same name.

---
+++ src/com/aremaitch/utils/NotImplementedException.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2010-2011 Roger Heim
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aremaitch.utils;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: roger
+ * Date: 4/14/11
+ * Time: 11:28 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class NotImplementedException extends RuntimeException {
+}
