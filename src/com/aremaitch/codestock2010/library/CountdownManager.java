2f388fee83900a2104a93bc60b372e1954ff55e1
roger <devnull@localhost>
2011-04-29 22:42:27 -0400

    Fixed stupid bug in countdown manager. Stopped the clock when it hit zero but went ahead and calculated/displayed negative numbers anyway. Also cleaned up some code cruft.

--- src/com/aremaitch/codestock2010/library/CountdownManager.java
+++ src/com/aremaitch/codestock2010/library/CountdownManager.java
@@ -17,7 +17,6 @@
 package com.aremaitch.codestock2010.library;
 
 import java.text.DecimalFormat;
-import java.text.NumberFormat;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.Timer;
@@ -51,20 +50,20 @@ public class CountdownManager {
     DecimalFormat df2digits = new DecimalFormat("00");
     DecimalFormat df2digitsnolead = new DecimalFormat("#0");
 
-//    private final String format2Digits = "%02d";                // make all digit parts have leading zeros
-//    private final String format3Digits = "%03d";
 
     private final Handler handler = new Handler();
     private final Runnable runnable = new Runnable() {
             @Override
             public void run() {
-//                ACLogger.info(CSConstants.LOG_TAG, "CountdownManager.TimerTask.Runnable.run()");
                 long diff = csStart - System.currentTimeMillis();
+
                 //	Cause you know someone will set their phone's clock ahead
                 //	just to see what happens...
                 if (diff <= 0) {
                     updateCountdownDisplay(0, 0, 0, 0);
                     stop();
+                    //  Such an idiot!
+                    return;
                 }
 
                 int iDays = Math.round(diff / msIn1Day);
@@ -88,6 +87,7 @@ public class CountdownManager {
 
     void updateCountdownDisplay(int iDays, int iHours, int iMinutes, int iSeconds) {
 
+        ACLogger.info(CSConstants.LOG_TAG, String.format("updateCountdownDisplay: iDays=%d iHours=%d iMinutes=%d iSeconds=%d", iDays, iHours, iMinutes, iSeconds));
         tvdays.setText(df3digitsnolead.format(iDays));                // no leading zeros; number can be 3, 2, or 1 digit
         tvhours.setText(df2digitsnolead.format(iHours));              // no leading zeros; number can be 2 or 1 digit
         tvminutes.setText(df2digitsnolead.format(iMinutes));          // no leading zeros; number can be 2 or 1 digit
@@ -98,10 +98,6 @@ public class CountdownManager {
         //  Still not great, but better.
         //  See http://groups.google.com/group/android-developers/browse_thread/thread/288ed6909459538e#
 
-//        tvdays.setText(String.format(format3Digits, iDays));
-//        tvhours.setText(String.format(format2Digits, iHours));
-//        tvminutes.setText(String.format(format2Digits, iMinutes));
-//        tvseconds.setText(String.format(format2Digits, iSeconds));
     }
 
     public void initializeCountdown(View digitsContainer, AssetManager assets) {
@@ -122,7 +118,6 @@ public class CountdownManager {
     public void start() {
         // start now and call tTask.run() every second
         tmr = new Timer();
-        ACLogger.info(CSConstants.LOG_TAG, "starting countdown timer");
         tmr.scheduleAtFixedRate(createNewTimerTask(), new Date(), 1000);
     }
 
@@ -130,43 +125,13 @@ public class CountdownManager {
         return new TimerTask() {
             @Override
             public void run() {
-//                ACLogger.info(CSConstants.LOG_TAG, "CountdownManager.TimerTask.run()");
                 handler.post(runnable);
-//                handler.post(new Runnable() {
-//                    @Override
-//                    public void run() {
-//                        ACLogger.info(CSConstants.LOG_TAG, "CountdownManager.TimerTask.Runnable.run()");
-//                        long diff = csStart - scheduledExecutionTime();
-//                        //	Cause you know someone will set their phone's clock ahead
-//                        //	just to see what happens...
-//                        if (diff <= 0) {
-//                            updateCountdownDisplay(0,0,0,0);
-//                            stop();
-//                        }
-//
-//                        int iDays = Math.round(diff / msIn1Day);
-//                        diff -= (iDays * msIn1Day);
-//
-//                        int iHours = Math.round(diff / msIn1Hour);
-//                        diff -= (iHours * msIn1Hour);
-//
-//                        int iMinutes = Math.round(diff / msIn1Minute);
-//                        diff -= (iMinutes * msIn1Minute);
-//
-//                        //	diff now contains the number of remaining seconds ( as milliseconds )
-//                        int iSeconds = Math.round(diff / 1000);
-//
-//                        updateCountdownDisplay(iDays, iHours, iMinutes, iSeconds);
-//
-//                    }
-//                });
             }
         };
     }
 
     public void stop() {
         //	so when we go to sleep, the clock stops
-        ACLogger.info(CSConstants.LOG_TAG, "stopping countdown timer");
 
         //  Once the timer is canceled nothing can be rescheduled on it.
         tmr.cancel();
